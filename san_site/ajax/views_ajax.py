834a5750342dd25282f3a1b8bb997aa1ec0db62e
mazaloff <mazaloff@bk.ru>
2018-09-01 17:17:47 +0300

    20180901

--- san_site/ajax/views_ajax.py
+++ san_site/ajax/views_ajax.py
@@ -1,8 +1,6 @@
-from san_site.models import Section, Product, Currency
+from san_site.models import Section
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.template.loader import render_to_string
-from django.db import connection
-from django.db.models.query import Prefetch
 import json
 
 
@@ -21,48 +19,13 @@ def get_goods(request):
         guid = request.GET.get('guid')
     except Section.DoesNotExist:
         raise HttpResponseBadRequest
+
+    try:
+        obj_Section = Section.objects.get(guid=guid)
+    except Section.DoesNotExist:
+        raise HttpResponseBadRequest
+
     return JsonResponse({
         "result": True,
-        'content': render_to_string('goods.html', {'goods_list': get_goods_list(guid)})
+        'content': render_to_string('goods.html', {'goods_list': obj_Section.get_goods_list()})
     })
-
-
-def get_goods_list(guid):
-
-    list_sections = []
-    create_list_sections(guid, list_sections)
-
-    products = Product.objects.filter(section__in=list_sections).order_by('code')\
-        .prefetch_related('product_inventories', 'product_prices')
-    currencis_dict = {elem_.id: elem_.name for elem_ in Currency.objects.all()}
-
-    list_res = []
-    for value_product in products:
-        quantity_sum = 0
-        for product_inventories in value_product.product_inventories.all():
-            quantity_sum += product_inventories.quantity
-        price_value = 0
-        currency_name = ''
-        for product_prices in value_product.product_prices.all():
-            price_value = product_prices.value
-            currency_name = currencis_dict.get(product_prices.currency_id, '')
-        list_res.append({'code': value_product.code,
-                         'name': value_product.name,
-                         'quantity': '>10' if quantity_sum > 10 else '' if quantity_sum == 0 else quantity_sum,
-                         'price': '' if price_value ==0 else price_value,
-                         'currency': currency_name})
-
-    return list_res
-
-
-def create_list_sections(object, list_sections):
-    if type(object) is str:
-        try:
-            object = Section.objects.get(guid=object)
-            list_sections.append(object)
-        except Section.DoesNotExist:
-            return
-    children = Section.objects.filter(parent_guid=object.guid)
-    for child in children:
-        list_sections.append(child)
-        create_list_sections(child, list_sections)
--- san_site/models.py
+++ san_site/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 
+class SectionManager(models.Manager):
+    pass
+
+
 class Section(models.Model):
     guid = models.CharField(max_length=50, db_index=True)
     name = models.CharField(max_length=200)
@@ -12,12 +16,48 @@ class Section(models.Model):
             default=timezone.now)
     is_deleted = models.BooleanField()
 
+    objects = SectionManager()
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.name
+
+    def list_with_children(self, list_sections):
+        list_sections.append(self)
+        children = Section.objects.filter(parent_guid=self.guid)
+        for child in children:
+            list_sections.append(child)
+            child.list_with_children(list_sections)
+
+    def get_goods_list(self):
+
+        list_sections = []
+        self.list_with_children(list_sections)
+
+        products = Product.objects.filter(section__in=list_sections).order_by('code') \
+            .prefetch_related('product_inventories', 'product_prices')
+        currencis_dict = {elem_.id: elem_.name for elem_ in Currency.objects.all()}
+
+        list_res = []
+        for value_product in products:
+            quantity_sum = 0
+            for product_inventories in value_product.product_inventories.all():
+                quantity_sum += product_inventories.quantity
+            price_value = 0
+            currency_name = ''
+            for product_prices in value_product.product_prices.all():
+                price_value = product_prices.value
+                currency_name = currencis_dict.get(product_prices.currency_id, '')
+            list_res.append({'code': value_product.code,
+                             'name': value_product.name,
+                             'quantity': '>10' if quantity_sum > 10 else '' if quantity_sum == 0 else quantity_sum,
+                             'price': '' if price_value == 0 else price_value,
+                             'currency': currency_name})
+
+        return list_res
 
 
 class Product(models.Model):
