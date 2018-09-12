52c22fe3b91dd79cbcdc380a0a0d7f3577938386
mazaloff <mazaloff@bk.ru>
2018-09-12 20:13:23 +0300

    20180914

--- Project/settings.py
+++ Project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import Project.local_settings as local_settings
+import Project.settings_local as local_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
