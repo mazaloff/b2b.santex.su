e56b8281b029511119bdd10e4c19aebd6be1e429
mazaloff <mazaloff@bk.ru>
2018-09-20 09:15:34 +0300

    20180920

--- Project/celery.py
+++ Project/celery.py
@@ -1,17 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
-from celery.schedules import crontab
 from django.conf import settings
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
-
-CELERY_BEAT_SCHEDULE = {
-    'orders-request-single-minute': {
-        'task': 'san_site.tasks.orders_request',
-        'schedule': crontab(minute=1)
-    }
-}
 
 app = Celery('san_site')
 app.config_from_object('django.conf:settings')
@@ -21,7 +13,7 @@ app.autodiscover_tasks()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(600.0, orders_request.s('orders_request'), name='orders_request')
+    sender.add_periodic_task(1800.0, orders_request.s('orders_request'), name='orders_request')
 
 
 @app.task
