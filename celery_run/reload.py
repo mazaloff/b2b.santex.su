ed5a7ae790ca804516dfa99dffa4a3a278bf7207
mazaloff <mazaloff@bk.ru>
2018-11-06 18:07:22 +0300

    20181106

--- celery_run/reload.py
+++ celery_run/reload.py
@@ -11,10 +11,12 @@ for i in list_:
     if p.name() in kill and os.getpid() != p.pid:
         p.terminate()
 
-subprocess.Popen('SCHTASKS /End /TN \celery\start_worker', shell=True, stdout=sys.stdout)
+subprocess.Popen('SCHTASKS /End /TN \celery\server', shell=True, stdout=sys.stdout)
 subprocess.Popen('SCHTASKS /End /TN \celery\start_beat', shell=True, stdout=sys.stdout)
 subprocess.Popen('SCHTASKS /End /TN \celery\start_flower', shell=True, stdout=sys.stdout)
+subprocess.Popen('SCHTASKS /End /TN \celery\start_flower', shell=True, stdout=sys.stdout)
 
-subprocess.Popen('SCHTASKS /Run /TN \celery\start_worker', shell=True, stdout=sys.stdout)
+subprocess.Popen('SCHTASKS /Run /TN \celery\server', shell=True, stdout=sys.stdout)
 subprocess.Popen('SCHTASKS /Run /TN \celery\start_beat', shell=True, stdout=sys.stdout)
+subprocess.Popen('SCHTASKS /Run /TN \celery\start_flower', shell=True, stdout=sys.stdout)
 subprocess.Popen('SCHTASKS /Run /TN \celery\start_flower', shell=True, stdout=sys.stdout)
