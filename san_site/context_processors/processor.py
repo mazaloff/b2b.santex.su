b7d8e330247e53ea6d508f3f91d68062200a475b
mazaloff <mazaloff@bk.ru>
2018-11-19 21:41:20 +0300

    customer

--- san_site/context_processors/processor.py
+++ san_site/context_processors/processor.py
@@ -19,4 +19,4 @@ def random_css(request):
     if settings.DEBUG:
         return {'random_css': random.randint(0, 99999999999999999)}
     else:
-        return {'random_css': ''}
+        return {'random_css': random.randint(0, 99999999999999999)}
