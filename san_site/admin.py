7aa7688ba8038b1fd965d12d4efc527d2ca970b4
mazaloff@bk.ru <mazaloff@bk.ru>
2019-08-25 21:31:37 +0300

    hasattr(v, 'lower') - fixed the issue

--- san_site/admin.py
+++ san_site/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Customer, Person, Order, OrderItem, Section, Product, Price, Store, Currency
 
@@ -23,8 +25,45 @@ class OrderItemInline(admin.TabularInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_filter = ['person', 'date', 'updated']
+    list_filter = ['date', 'customer', 'updated']
     inlines = [OrderItemInline]
+
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+
+# Register out own model admin, based on the default UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = [
+        'date_joined',
+    ]
+
+    def get_readonly_fields(self, request, obj=None):
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'password',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        # Prevent non-superusers from editing their own permissions
+        if (
+                not is_superuser
+                and obj is not None
+                and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+        return disabled_fields
 
 
 admin.site.register(Order, OrderAdmin)
