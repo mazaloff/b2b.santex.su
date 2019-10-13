ca85cb1d90fa199ada18ec29e73a931a731663ef
mazaloff@bk.ru <mazaloff@bk.ru>
2019-10-13 20:07:55 +0300

    new the hosting

--- Project/settings.py
+++ Project/settings.py
@@ -170,10 +170,10 @@ API_URL = 'http://185.46.154.84:8080/UprTest/hs/'
 
 # Celery Data Format
 
-BROKER_URL = 'amqp://guest:guest@localhost:6379/0'
+BROKER_URL = 'amqp://guest:guest@localhost:6379'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
