ac285f36b3064e9b78fb4e81648843ced941bf99
mazaloff@bk.ru <mazaloff@bk.ru>
2019-10-19 22:34:04 +0300

    fix create the order

--- Project/server_api.py
+++
@@ -1,6 +0,0 @@
-from Project.server import server_wsgi
-import Project.settings_local as settings
-
-if __name__ == '__main__':
-    name_server = 'exchange'
-    server_wsgi(settings.SERVER_ARI, 10, f'server{name_server}')
--- Project/server_nginx.py
+++
@@ -1,6 +0,0 @@
-from Project.server import server_wsgi
-import Project.settings_local as settings
-
-if __name__ == '__main__':
-    name_server = 'nginx'
-    server_wsgi(settings.SERVER_NGINX, 2, f'server{name_server}')
