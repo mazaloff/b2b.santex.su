61c8c6e49a9d3f5c42f1f4e0a2164f5e99d03159
mazaloff@bk.ru <mazaloff@bk.ru>
2020-02-17 16:30:31 +0300

    new version API rest products

--- san_rest_api/serializers.py
+++ san_rest_api/serializers.py
@@ -3,7 +3,12 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from san_site.models import Product
+from san_site.models import Product, Currency
+
+courses = {}
+currency = Currency.objects.all()
+for elem in currency:
+    courses[elem.id] = elem.get_today_course()
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -63,6 +68,76 @@ class ProductSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def calculate_rrp(instance):
+        return instance.price_rrp
+
+    @staticmethod
+    def calculate_photo(instance):
+        return '' if instance.image.name == '' else settings.URL + resolve_url(instance.image.url)
+
+
+class ProductSerializerV1(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(method_name='calculate_id')
+    article = serializers.SerializerMethodField(method_name='calculate_article')
+    barcode = serializers.SerializerMethodField(method_name='calculate_barcode')
+    name = serializers.SerializerMethodField(method_name='calculate_name')
+    matrix = serializers.SerializerMethodField(method_name='calculate_matrix')
+    quantity = serializers.SerializerMethodField(method_name='calculate_quantity')
+    photo = serializers.SerializerMethodField(method_name='calculate_photo')
+    brand = serializers.SerializerMethodField(method_name='calculate_brand')
+    price = serializers.SerializerMethodField(method_name='calculate_price')
+    currency = serializers.SerializerMethodField(method_name='calculate_currency')
+    price_rub = serializers.SerializerMethodField(method_name='calculate_price_rub')
+    rrp_rub = serializers.SerializerMethodField(method_name='calculate_rrp_rub')
+
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'article', 'name', 'brand', 'barcode', 'matrix', 'photo', 'quantity', 'price',
+            'currency', 'price_rub', 'rrp_rub')
+
+    @staticmethod
+    def calculate_name(instance):
+        return instance.name_
+
+    @staticmethod
+    def calculate_id(instance):
+        return instance.guid_
+
+    @staticmethod
+    def calculate_barcode(instance):
+        return instance.barcode_
+
+    @staticmethod
+    def calculate_matrix(instance):
+        return instance.matrix_
+
+    @staticmethod
+    def calculate_article(instance):
+        return instance.article_
+
+    @staticmethod
+    def calculate_brand(instance):
+        return instance.brand_name_
+
+    @staticmethod
+    def calculate_quantity(instance):
+        return 10 if instance.quantity > 10 else instance.quantity
+
+    @staticmethod
+    def calculate_price(instance):
+        return instance.price
+
+    @staticmethod
+    def calculate_currency(instance):
+        return instance.currency
+
+    @staticmethod
+    def calculate_price_rub(instance):
+        course = courses.get(instance.currency_id, {'course': 1, 'multiplicity': 1})
+        return round(instance.price * course['course'] / course['multiplicity'], 2)
+
+    @staticmethod
+    def calculate_rrp_rub(instance):
         return instance.price_rrp
 
     @staticmethod
--- san_rest_api/views.py
+++ san_rest_api/views.py
@@ -23,7 +23,7 @@ from rest_framework.status import (
 from san_site.models import Brand, Product, Person, CustomersFiles, Currency, get_customer
 from san_site.decorates.decorate import page_not_access
 
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductSerializerV1
 
 
 class ProductListView(APIView):
@@ -98,6 +98,7 @@ class ProductListView(APIView):
             f"""WITH result AS (
                 SELECT _product.id AS id,
                         _product.code AS code_,
+                        _product.guid AS guid_,
                         _product.code_brand AS article_,
                         _product.barcode AS barcode_,
                         _product.name AS name_,
@@ -140,6 +141,138 @@ class ProductListView(APIView):
                 FROM result
                 ORDER BY result.code_;""", param)
         serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+
+class ProductListViewV1(APIView):
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
+    @staticmethod
+    @api_view(('GET',))
+    def get(request):
+
+        current_customer = get_customer(request.user)
+        current_customer_id = None
+        if current_customer:
+            current_customer_id = current_customer.id
+
+        if current_customer_id is None:
+            return Response({'error': 'Не удалось авторизовать пользователя'},
+                            status=HTTP_403_FORBIDDEN)
+
+        param = [current_customer_id]
+
+        filter_id = ''
+
+        for key, value in request.GET.items():
+            if key.startswith('filter_id'):
+                filter_id += f"{'' if filter_id == '' else ','}{value}"
+
+        filter_code = ''
+
+        for key, value in request.GET.items():
+            if key.startswith('filter_code'):
+                filter_code += f"{'' if filter_code == '' else ','}{value}"
+
+        filter_article = ''
+
+        for key, value in request.GET.items():
+            if key.startswith('filter_article'):
+                filter_article += f"{'' if filter_article == '' else ','}{value}"
+
+        filter_barcode = ''
+
+        for key, value in request.GET.items():
+            if key.startswith('filter_barcode'):
+                filter_barcode += f"{'' if filter_barcode == '' else ','}{value}"
+
+        filter_brand = ''
+
+        for key, value in request.GET.items():
+            if key.startswith('filter_brand'):
+                filter_brand += f"{'' if filter_brand == '' else ','}{value}"
+
+        filter_quantity = request.GET.get('filter_quantity', '')
+
+        str_filter_id = ' TRUE '
+        if filter_id != '':
+            param += [list(map(lambda x: x.upper(), str_filter_id.split(','))), ]
+            str_filter_id = 'UPPER(_product.guid::text) = ANY(%s)'
+
+        str_filter_code = ' TRUE '
+        if filter_code != '':
+            param += [list(map(lambda x: x.upper(), filter_code.split(','))), ]
+            str_filter_code = 'UPPER(_product.code::text) = ANY(%s)'
+
+        str_filter_article = ' TRUE '
+        if filter_article != '':
+            param += [list(map(lambda x: x.upper(), filter_article.split(','))), ]
+            str_filter_article = 'UPPER(_product.code_brand::text) = ANY(%s)'
+
+        str_filter_barcode = ' TRUE '
+        if filter_barcode != '':
+            param += [list(map(lambda x: x.upper(), filter_barcode.split(','))), ]
+            str_filter_barcode = 'UPPER(_product.barcode::text) = ANY(%s)'
+
+        str_filter_brand = ' TRUE '
+        if filter_brand != '':
+            param += [list(map(lambda x: x.upper(), filter_brand.split(','))), ]
+            str_filter_brand = 'UPPER(_brand.name::text) = ANY(%s)'
+
+        str_filter_quantity = ' TRUE '
+        if filter_quantity != '' and filter_quantity.upper() == 'YES':
+            str_filter_quantity = 'COALESCE(_inventories.quantity, 0) > 0'
+
+        queryset = Product.objects.raw(
+            f"""WITH result AS (
+                SELECT _product.id AS id,
+                        _product.code AS code_,
+                        _product.guid AS guid_,
+                        _product.code_brand AS article_,
+                        _product.barcode AS barcode_,
+                        _product.name AS name_,
+                        _product.matrix AS matrix_,
+                        _product.image AS image,
+                        COALESCE(_brand.name, '') AS brand_name_,
+                        COALESCE(_customersprices.discount, COALESCE(_prices.value, 0)) AS price,
+                        COALESCE(_customersprices_cur.name, COALESCE(_prices_cur.name, '')) AS currency,
+                        COALESCE(_customersprices_cur.id, COALESCE(_prices_cur.id, 0)) AS currency_id,
+                        COALESCE(_prices.rrp, 0) AS price_rrp,
+                        SUM(COALESCE(_inventories.quantity, 0)) AS quantity
+                    FROM san_site_product _product
+                        LEFT JOIN san_site_prices _prices ON _product.id = _prices.product_id
+                            LEFT JOIN san_site_currency _prices_cur ON _prices.currency_id = _prices_cur.id
+                        LEFT JOIN san_site_customersprices _customersprices 
+                            ON _customersprices.customer_id = %s AND _product.id = _customersprices.product_id  
+                                LEFT JOIN san_site_currency _customersprices_cur 
+                                    ON _customersprices.currency_id = _customersprices_cur.id
+                        LEFT JOIN san_site_inventories _inventories ON _product.id = _inventories.product_id
+                        LEFT JOIN san_site_brand _brand ON _product.brand_id = _brand.id
+                    WHERE _product.is_deleted = FALSE
+                        AND {str_filter_id}
+                        AND {str_filter_code}
+                        AND {str_filter_article}
+                        AND {str_filter_brand}
+                        AND {str_filter_barcode}
+                        AND {str_filter_quantity}
+                    GROUP BY _product.id,
+                        _product.code,
+                        _product.code_brand,
+                        _product.barcode,
+                        _product.name,
+                        _product.matrix,
+                        _product.image,
+                        COALESCE(_brand.name, ''),
+                        COALESCE(_prices.value, 0),
+                        COALESCE(_customersprices.discount, COALESCE(_prices.value, 0)),
+                        COALESCE(_customersprices_cur.name, COALESCE(_prices_cur.name, '')),
+                        COALESCE(_customersprices_cur.id, COALESCE(_prices_cur.id, 0)),
+                        COALESCE(_prices.rrp, 0)
+                    )
+                SELECT *
+                FROM result
+                ORDER BY result.code_;""", param)
+        serializer = ProductSerializerV1(queryset, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
 
