3def88642d9baa6def48aefeede80cce78c42130
mazaloff <mazaloff@bk.ru>
2022-09-10 21:02:50 +0300

    remove test2

--- Project/urls.py
+++ Project/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
-#
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('san_rest_api.urls')),
