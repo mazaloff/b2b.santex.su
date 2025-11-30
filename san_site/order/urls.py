2ae7dba5b96145155b105d6bf1a9dc348ddef599
mazaloff <mazaloff@bk.ru>
2025-11-30 12:03:35 +0300

    test connection

--- san_site/order/urls.py
+++ san_site/order/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from san_site.order import views
-
+#
 urlpatterns = [
     url(r'^request[/](?P<id>\d+)[/]?$', views.order_request, name='order_request'),
     url(r'^(list[/])?(?P<id>\d+)[/]?$', views.order, name='order'),
